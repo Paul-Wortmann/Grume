46f25ab4b322f105d99bd213d4b6baab1b357ec4
Paul-Wortmann <paul.wortmann@gmail.com>
2012-09-03 15:13:48 +0900

    Updated change log.

--- source/main.cpp
+++ source/main.cpp
@@ -29,7 +29,7 @@
 
 extern game_class game;
 
-const char application_name[] = ("Frost and Flame V0.10 - www.physhexgames.co.nr");
+const char application_name[] = ("Frost and Flame V0.11 - www.physhexgames.co.nr");
 const char application_icon[] = ("data/icon.bmp");
 
 Uint32                   colorkey;
