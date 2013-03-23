7c3f9402da20442146fab505cdd0e8f272baf274
Paul-Wortmann <paul.wortmann@gmail.com>
2013-03-23 17:45:57 +0900

    fixes to horizonltal node splitting....

--- source/core/map_gen.cpp
+++ source/core/map_gen.cpp
@@ -1,25 +1,25 @@
 /**
- * Copyright (C) 2011-2013 Paul Wortmann, PhysHex Games, www.physhexgames.co.nr
- * This file is part of "Frost And Flame"
+ * Copyright (C) 2013 Paul Wortmann, paul.wormann@gmail.com.
+ * This file is part of "CS50 - project -> RDG(Random Dungeon Generator)"
  *
- * "Frost And Flame" is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * "CS50 - project -> RDG(Random Dungeon Generator)" is free software: you
+ * can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
  *
- * "Frost And Flame" is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * "CS50 - project -> RDG(Random Dungeon Generator)" is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with "Frost And Flame" If not, see <http://www.gnu.org/licenses/>.
+ * along with "CS50 - project -> RDG(Random Dungeon Generator)" If not,
+ * see <http://www.gnu.org/licenses/>.
  *
  * @author  Paul Wortmann
- * @email   physhex@gmail.com
- * @website www.physhexgames.co.nr
+ * @email   paul.wortmann@gmail.com
  * @license GPL V3
- * @date 2011-11-11
+ * @date 2013-03-11
  */
 
 #include "map_gen.hpp"
@@ -27,6 +27,8 @@
 #include <stdlib.h>
 
 map_type rdg_map;
+
+int room_count = 0;
 
 void map_gen_init(int size_x, int size_y)
 {
@@ -54,6 +56,10 @@ void map_gen_split(map_node_type *map_node)
     int tile_data_count  = 0;
     int  x_range = map_node->data.size.x  - (ROOM_MAX_X*2);
     int  y_range = map_node->data.size.y  - (ROOM_MAX_Y*2);
+
+    //could determine split based on size_x < size_y etc...
+
+    /*
     bool split_x = (x_range > 0) ? true : false;
     bool split_y = (y_range > 0) ? true : false;
     if (split_x && split_y)
@@ -61,79 +67,17 @@ void map_gen_split(map_node_type *map_node)
         if (rand()%100 > 50) split_x = false;
         else split_y = false;
     }
-    if (split_y) // split horizontally
-    {
-        y_range = (y_range > 0) ? rand()%y_range : 0;
-        int new_size_y_1 = ROOM_MAX_Y + y_range + (map_node->data.size.y % 2);
-        int new_size_y_2 = map_node->data.size.y - new_size_y_1;
-        // left --------------------------------------------------------------------------------------------------------
-        map_node->left = new map_node_type;
-        map_node->left->leaf = false;
-        map_node->left->data.size.x = map_node->data.size.x;
-        map_node->left->data.size.y = new_size_y_1;
-        map_node->left->data.number_of_tiles = map_node->left->data.size.x * map_node->left->data.size.y;
-        map_node->left->data.tile = new tile_type[(map_node->left->data.size.x*map_node->left->data.size.y)+1];
-        tile_count_x_out = 0;
-        tile_count_y_out = 0;
-        tile_data_count  = 0;
-        for(int tile_count = 0; tile_count < map_node->left->data.number_of_tiles; tile_count++)
-        {
-            tile_data_count = (tile_count_y_out*map_node->data.size.x)+tile_count_x_out+map_node->data.tile[0].position.x;
-            map_node->left->data.tile[tile_count].position.x = map_node->data.tile[tile_data_count].position.x;
-            map_node->left->data.tile[tile_count].position.y = map_node->data.tile[tile_data_count].position.y;
-            map_node->left->data.tile[tile_count].layer = 1;//test
-            tile_count_x_out++;
-            if (tile_count_x_out >= map_node->left->data.size.x)
-            {
-                tile_count_x_out = 0;
-                tile_count_y_out++;
-            }
-        }
-        map_gen_split(map_node->left);
-        for(int tile_count = 0; tile_count < map_node->left->data.number_of_tiles; tile_count++)
-        {
-            for(int tile_data_count = 0; tile_data_count < map_node->data.number_of_tiles; tile_data_count++)
-            {
-                if ((map_node->data.tile[tile_data_count].position.x == map_node->left->data.tile[tile_count].position.x) &&
-                    (map_node->data.tile[tile_data_count].position.y == map_node->left->data.tile[tile_count].position.y))
-                     map_node->data.tile[tile_data_count].layer       = map_node->left->data.tile[tile_count].layer;
-            }
-        }
-        delete [] map_node->left;
-        // right --------------------------------------------------------------------------------------------------------
-        map_node->right = new map_node_type;
-        map_node->right->leaf = false;
-        map_node->right->data.size.x = map_node->data.size.x;
-        map_node->right->data.size.y = new_size_y_2;
-        map_node->right->data.number_of_tiles = map_node->right->data.size.x * map_node->right->data.size.y;
-        map_node->right->data.tile = new tile_type[(map_node->right->data.size.x*map_node->right->data.size.y)+1];
-        tile_count_x_out = 0;
-        tile_count_y_out = new_size_y_1;
-        tile_data_count  = 0;
-        for(int tile_count = 0; tile_count < map_node->right->data.number_of_tiles; tile_count++)
-        {
-            tile_data_count = (tile_count_y_out*map_node->data.size.x)+tile_count_x_out+map_node->data.tile[0].position.x;
-            map_node->right->data.tile[tile_count].position.x = map_node->data.tile[tile_data_count].position.x;
-            map_node->right->data.tile[tile_count].position.y = map_node->data.tile[tile_data_count].position.y;
-            map_node->right->data.tile[tile_count].layer = 2;//test
-            tile_count_x_out++;
-            if (tile_count_x_out >= map_node->right->data.size.x)
-            {
-                tile_count_x_out = 0;
-                tile_count_y_out++;
-            }
-        }
-        for(int tile_count = 0; tile_count < map_node->right->data.number_of_tiles; tile_count++)
-        {
-            for(int tile_data_count = 0; tile_data_count < map_node->data.number_of_tiles; tile_data_count++)
-            {
-                if ((map_node->data.tile[tile_data_count].position.x == map_node->right->data.tile[tile_count].position.x) &&
-                    (map_node->data.tile[tile_data_count].position.y == map_node->right->data.tile[tile_count].position.y))
-                     map_node->data.tile[tile_data_count].layer       = map_node->right->data.tile[tile_count].layer;
-            }
-        }
-        delete [] map_node->right;
-    }
+    */
+    bool split_x = true;
+    bool split_y = false;
+    if (x_range <= 0) split_x = false;
+    /*
+    bool split_x = false;
+    bool split_y = true;
+    if (y_range <= 0) split_y = false;
+    */
+
+    if (room_count >= MAX_ROOMS) split_x = split_y = false;
     if (split_x) // split vertically
     {
         x_range = (x_range > 0) ? rand()%x_range : 0;
@@ -145,16 +89,17 @@ void map_gen_split(map_node_type *map_node)
         map_node->left->data.size.x = new_size_x_1;
         map_node->left->data.size.y = map_node->data.size.y;
         map_node->left->data.number_of_tiles = map_node->left->data.size.x * map_node->left->data.size.y;
-        map_node->left->data.tile = new tile_type[(map_node->left->data.size.x*map_node->left->data.size.y)+1];
+        map_node->left->data.tile = new tile_type[map_node->left->data.number_of_tiles];
         tile_count_x_out = 0;
         tile_count_y_out = 0;
         tile_data_count  = 0;
+        room_count++;
         for(int tile_count = 0; tile_count < map_node->left->data.number_of_tiles; tile_count++)
         {
             tile_data_count = (tile_count_y_out*map_node->data.size.x)+tile_count_x_out+map_node->data.tile[0].position.x;
             map_node->left->data.tile[tile_count].position.x = map_node->data.tile[tile_data_count].position.x;
             map_node->left->data.tile[tile_count].position.y = map_node->data.tile[tile_data_count].position.y;
-            map_node->left->data.tile[tile_count].layer = 3;//test
+            map_node->left->data.tile[tile_count].layer = room_count;//test
             tile_count_x_out++;
             if (tile_count_x_out >= new_size_x_1)
             {
@@ -165,7 +110,7 @@ void map_gen_split(map_node_type *map_node)
         map_gen_split(map_node->left);
         for(int tile_count = 0; tile_count < map_node->left->data.number_of_tiles; tile_count++)
         {
-            for(int tile_data_count = 0; tile_data_count < map_node->data.number_of_tiles; tile_data_count++)
+            for(tile_data_count = 0; tile_data_count < map_node->data.number_of_tiles; tile_data_count++)
             {
                 if ((map_node->data.tile[tile_data_count].position.x == map_node->left->data.tile[tile_count].position.x) &&
                     (map_node->data.tile[tile_data_count].position.y == map_node->left->data.tile[tile_count].position.y))
@@ -179,16 +124,17 @@ void map_gen_split(map_node_type *map_node)
         map_node->right->data.size.x = new_size_x_2;
         map_node->right->data.size.y = map_node->data.size.y;
         map_node->right->data.number_of_tiles = map_node->right->data.size.x * map_node->right->data.size.y;
-        map_node->right->data.tile = new tile_type[(map_node->right->data.size.x*map_node->right->data.size.y)+1];
+        map_node->right->data.tile = new tile_type[map_node->right->data.number_of_tiles];
         tile_count_x_out = new_size_x_1;
         tile_count_y_out = 0;
         tile_data_count  = 0;
+        room_count++;
         for(int tile_count = 0; tile_count < map_node->right->data.number_of_tiles; tile_count++)
         {
             tile_data_count = (tile_count_y_out*map_node->data.size.x)+tile_count_x_out+map_node->data.tile[0].position.x;
             map_node->right->data.tile[tile_count].position.x = map_node->data.tile[tile_data_count].position.x;
             map_node->right->data.tile[tile_count].position.y = map_node->data.tile[tile_data_count].position.y;
-            map_node->right->data.tile[tile_count].layer = 4;//test
+            map_node->right->data.tile[tile_count].layer = room_count;//test
             tile_count_x_out++;
             if (tile_count_x_out >= map_node->data.size.x)
             {
@@ -196,9 +142,86 @@ void map_gen_split(map_node_type *map_node)
                 tile_count_y_out++;
             }
         }
+        map_gen_split(map_node->right);
         for(int tile_count = 0; tile_count < map_node->right->data.number_of_tiles; tile_count++)
         {
-            for(int tile_data_count = 0; tile_data_count < map_node->data.number_of_tiles; tile_data_count++)
+            for(tile_data_count = 0; tile_data_count < map_node->data.number_of_tiles; tile_data_count++)
+            {
+                //if ((map_node->data.tile[tile_data_count].position.x == map_node->right->data.tile[tile_count].position.x) &&
+                //    (map_node->data.tile[tile_data_count].position.y == map_node->right->data.tile[tile_count].position.y))
+                //     map_node->data.tile[tile_data_count].layer       = map_node->right->data.tile[tile_count].layer;
+            }
+        }
+        delete [] map_node->right;
+    }
+    if (split_y) // split horizontally
+    {
+        y_range = (y_range > 0) ? rand()%y_range : 0;
+        int new_size_y_1 = ROOM_MAX_Y + y_range + (map_node->data.size.y % 2);
+        int new_size_y_2 = map_node->data.size.y - new_size_y_1;
+        // left --------------------------------------------------------------------------------------------------------
+        map_node->left = new map_node_type;
+        map_node->left->leaf = false;
+        map_node->left->data.size.x = map_node->data.size.x;
+        map_node->left->data.size.y = new_size_y_1;
+        map_node->left->data.number_of_tiles = map_node->left->data.size.x * map_node->left->data.size.y;
+        map_node->left->data.tile = new tile_type[map_node->left->data.number_of_tiles];
+        tile_count_x_out = 0;
+        tile_count_y_out = 0;
+        tile_data_count  = 0;
+        room_count++;
+        for(int tile_count = 0; tile_count < map_node->left->data.number_of_tiles; tile_count++)
+        {
+            tile_data_count = (tile_count_y_out*map_node->data.size.x)+tile_count_x_out+map_node->data.tile[0].position.x;
+            map_node->left->data.tile[tile_count].position.x = map_node->data.tile[tile_data_count].position.x;
+            map_node->left->data.tile[tile_count].position.y = map_node->data.tile[tile_data_count].position.y;
+            map_node->left->data.tile[tile_count].layer = room_count;//test
+            tile_count_x_out++;
+            if (tile_count_x_out >= map_node->left->data.size.x)
+            {
+                tile_count_x_out = 0;
+                tile_count_y_out++;
+            }
+        }
+        map_gen_split(map_node->left);
+        for(int tile_count = 0; tile_count < map_node->left->data.number_of_tiles; tile_count++)
+        {
+            for(tile_data_count = 0; tile_data_count < map_node->data.number_of_tiles; tile_data_count++)
+            {
+                if ((map_node->data.tile[tile_data_count].position.x == map_node->left->data.tile[tile_count].position.x) &&
+                    (map_node->data.tile[tile_data_count].position.y == map_node->left->data.tile[tile_count].position.y))
+                     map_node->data.tile[tile_data_count].layer       = map_node->left->data.tile[tile_count].layer;
+            }
+        }
+        delete [] map_node->left;
+        // right --------------------------------------------------------------------------------------------------------
+        map_node->right = new map_node_type;
+        map_node->right->leaf = false;
+        map_node->right->data.size.x = map_node->data.size.x;
+        map_node->right->data.size.y = new_size_y_2;
+        map_node->right->data.number_of_tiles = map_node->right->data.size.x * map_node->right->data.size.y;
+        map_node->right->data.tile = new tile_type[map_node->right->data.number_of_tiles];
+        tile_count_x_out = 0;
+        tile_count_y_out = new_size_y_1;
+        tile_data_count  = 0;
+        room_count++;
+        for(int tile_count = 0; tile_count < map_node->right->data.number_of_tiles; tile_count++)
+        {
+            tile_data_count = (tile_count_y_out*map_node->data.size.x)+tile_count_x_out+map_node->data.tile[0].position.x;
+            map_node->right->data.tile[tile_count].position.x = map_node->data.tile[tile_data_count].position.x;
+            map_node->right->data.tile[tile_count].position.y = map_node->data.tile[tile_data_count].position.y;
+            map_node->right->data.tile[tile_count].layer = room_count;//test
+            tile_count_x_out++;
+            if (tile_count_x_out >= map_node->right->data.size.x)
+            {
+                tile_count_x_out = 0;
+                tile_count_y_out++;
+            }
+        }
+        map_gen_split(map_node->right);
+        for(int tile_count = 0; tile_count < map_node->right->data.number_of_tiles; tile_count++)
+        {
+            for(tile_data_count = 0; tile_data_count < map_node->data.number_of_tiles; tile_data_count++)
             {
                 if ((map_node->data.tile[tile_data_count].position.x == map_node->right->data.tile[tile_count].position.x) &&
                     (map_node->data.tile[tile_data_count].position.y == map_node->right->data.tile[tile_count].position.y))
@@ -209,6 +232,7 @@ void map_gen_split(map_node_type *map_node)
     }
     if (!split_x && !split_y)
     {
+        printf("Node start -> %d x %d y\n",map_node->data.tile[0].position.x,map_node->data.tile[0].position.y);
         map_node->leaf = true;
         // gen_room code goes here
         // passage gen here?
@@ -236,10 +260,11 @@ void map_gen_display(void)
     {
         for (int tile_count_x = 0; tile_count_x < rdg_map.size.x; tile_count_x++)
         {
-            printf("%d",rdg_map.tile[(tile_count_y * rdg_map.size.x) + tile_count_x].layer);
+            printf("%c",(char)rdg_map.tile[(tile_count_y * rdg_map.size.x) + tile_count_x].layer+'a');
         }
         if (rdg_map.size.x < 80) printf("\n");
     }
+    printf("Room count -> %d\n",room_count);
 };
 
 
--- source/core/map_gen.hpp
+++ source/core/map_gen.hpp
@@ -1,31 +1,32 @@
 /**
- * Copyright (C) 2011-2013 Paul Wortmann, PhysHex Games, www.physhexgames.co.nr
- * This file is part of "Frost And Flame"
+ * Copyright (C) 2013 Paul Wortmann, paul.wormann@gmail.com.
+ * This file is part of "CS50 - project -> RDG(Random Dungeon Generator)"
  *
- * "Frost And Flame" is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * "CS50 - project -> RDG(Random Dungeon Generator)" is free software: you
+ * can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
  *
- * "Frost And Flame" is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * "CS50 - project -> RDG(Random Dungeon Generator)" is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with "Frost And Flame" If not, see <http://www.gnu.org/licenses/>.
+ * along with "CS50 - project -> RDG(Random Dungeon Generator)" If not,
+ * see <http://www.gnu.org/licenses/>.
  *
  * @author  Paul Wortmann
- * @email   physhex@gmail.com
- * @website www.physhexgames.co.nr
+ * @email   paul.wortmann@gmail.com
  * @license GPL V3
- * @date 2011-11-11
+ * @date 2013-03-11
  */
 
 #ifndef MAP_GEN_HPP
 #define MAP_GEN_HPP
 
 // ROOM_MAX must be an even number!
+#define MAX_ROOMS  64
 #define ROOM_MAX_X 4
 #define ROOM_MAX_Y 4
 
