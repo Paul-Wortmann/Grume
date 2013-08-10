44e4c392d0d58e3e6d06eec0d1334a6545e3a179
Paul-Wortmann <paul.wortmann@gmail.com>
2013-08-10 12:18:51 +0900

    Version bump V0.22

--- source/main.cpp
+++ source/main.cpp
@@ -29,7 +29,7 @@
 
 extern game_class    game;
 
-const char application_name[] = ("Frost and Flame V0.21 - www.physhexgames.co.nr");
+const char application_name[] = ("Frost and Flame V0.22 - www.physhexgames.co.nr");
 const char application_icon[] = ("data/icon.bmp");
 
 Uint32                   colorkey;
