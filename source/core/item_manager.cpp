351f81cffd6c8a6c9b86721097ce8440f8530636
Paul-Wortmann <paul.wortmann@gmail.com>
2013-10-30 22:31:49 +0900

    Random name generation for ITEM_OFFHAND.

--- source/core/item_manager.cpp
+++ source/core/item_manager.cpp
@@ -2271,6 +2271,117 @@ void  item_manager_class::gen_item_name(item_type* item_pointer,int item_type_UI
                     base_name = "shield ";
                 break;
             }
+            switch (best_effect)
+            {
+                case EFFECT_MOD_STRENGTH:
+                    post_name = "of power";
+                break;
+                case EFFECT_MOD_DEXTERITY:
+                    post_name = "of nimbus";
+                break;
+                case EFFECT_MOD_INTELLECT:
+                    post_name = "of the mind";
+                break;
+                case EFFECT_MOD_ALL_ATTRIBUTES:
+                    post_name = "of skill";
+                break;
+                case EFFECT_MOD_RESIST_PHYSICAL:
+                    post_name = "of the brute";
+                break;
+                case EFFECT_MOD_RESIST_ICE:
+                    post_name = "of frost";
+                break;
+                case EFFECT_MOD_RESIST_FIRE:
+                    post_name = "of flame";
+                break;
+                case EFFECT_MOD_RESIST_LIGHTNING:
+                    post_name = "of thunder";
+                break;
+                case EFFECT_MOD_RESIST_ALL:
+                    post_name = "of resistance";
+                break;
+                case EFFECT_MOD_HEALTH_REGEN:
+                    post_name = "of life";
+                break;
+                case EFFECT_MOD_HEALTH_MAX:
+                    post_name = "of constitution";
+                break;
+                case EFFECT_MOD_HEALTH_HIT:
+                    post_name = "of life leach";
+                break;
+                case EFFECT_MOD_HEALTH_KILL:
+                    post_name = "of life steal";
+                break;
+                case EFFECT_MOD_MANA_REGEN:
+                    post_name = "of hexing";
+                break;
+                case EFFECT_MOD_MANA_MAX:
+                    post_name = "of wizardry";
+                break;
+                case EFFECT_MOD_MANA_HIT:
+                    post_name = "of mana drain";
+                break;
+                case EFFECT_MOD_MANA_KILL:
+                    post_name = "of mana steal";
+                break;
+                case EFFECT_MOD_MOVEMENT_SPEED:
+                    post_name = "of speed";
+                break;
+                case EFFECT_MOD_MAGIC_FIND:
+                    post_name = "of luck";
+                break;
+                case EFFECT_MOD_GOLD_FIND:
+                    post_name = "of the leprechaun";
+                break;
+                case EFFECT_MOD_LIGHT_RADIUS:
+                    post_name = "of brilliance";
+                break;
+                case EFFECT_MOD_EXP_PER_KILL:
+                    post_name = "of wisdom";
+                break;
+                case EFFECT_MOD_SPELL_CAST_DMG:
+                    post_name = "of reflection";
+                break;
+                case EFFECT_MOD_SPELL_CAST_HIT:
+                    post_name = "of magic";
+                break;
+                case EFFECT_MOD_SPELL_CAST_KILL:
+                    post_name = "of spawning";
+                break;
+                case EFFECT_MOD_SPELL:
+                    post_name = "of Beltane";
+                break;
+                case EFFECT_MOD_SPELL_TYPE:
+                    post_name = "of Samhain";
+                break;
+                case EFFECT_MOD_SPELL_ALL:
+                    post_name = "of the Coven";
+                break;
+                default:
+                    switch (random(6))
+                    {
+                        case 0:
+                            post_name = "of blocking";
+                        break;
+                        case 1:
+                            post_name = "of warding";
+                        break;
+                        case 2:
+                            post_name = "of stopping";
+                        break;
+                        case 3:
+                            post_name = "of safety";
+                        break;
+                        case 4:
+                            post_name = "of misery";
+                        break;
+                        case 5:
+                        default:
+                            post_name = "of woe";
+                        break;
+                    }
+                break;
+            }
             item_pointer->data.name = pre_name+base_name+post_name;
         break;
         case ITEM_WEAPON:
@@ -2673,3 +2784,8 @@ void  item_manager_class::gen_item_sounds(item_type* item_pointer,int item_type_
                 break;
                 case EFFECT_MOD_ARMOR:
 */
+
+
+
+
+
