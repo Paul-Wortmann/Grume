9a523c716d15734eae70e5a0a2e5008e7ee7ed53
Paul-Wortmann <paul.wortmann@gmail.com>
2013-11-14 23:28:24 +0900

    Bows to use random texture.

--- source/core/item_manager.cpp
+++ source/core/item_manager.cpp
@@ -3086,7 +3086,11 @@ void  item_manager_class::gen_item_texture(item_type* item_pointer,int item_type
                 break;
                 case ITEM_BOW:
                     item_pointer->data.material_type = ITEM_MATERIAL_WOOD;
-                    item_pointer->data.image.level_0 = game.texture_manager.add_texture("data/textures/UI/icons/bows/bow_03.png");
+                    temp_int = random(5);
+                    temp_string = "data/textures/UI/icons/bows/bow_";
+                    if (temp_int < 10) temp_string += "0";
+                    temp_string += int_to_string(temp_int) + ".png";
+                    item_pointer->data.image.level_0 = game.texture_manager.add_texture(temp_string);
                 break;
                 case ITEM_MACE:
                     item_pointer->data.material_type = ITEM_MATERIAL_METAL;
