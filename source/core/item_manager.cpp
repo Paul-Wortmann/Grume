4529ae969be657f483a36d576bd34c22877cc6ea
Paul-Wortmann <paul.wortmann@gmail.com>
2013-11-14 23:29:17 +0900

    Maces to use random texture.

--- source/core/item_manager.cpp
+++ source/core/item_manager.cpp
@@ -3094,7 +3094,11 @@ void  item_manager_class::gen_item_texture(item_type* item_pointer,int item_type
                 break;
                 case ITEM_MACE:
                     item_pointer->data.material_type = ITEM_MATERIAL_METAL;
-                    item_pointer->data.image.level_0 = game.texture_manager.add_texture("data/textures/UI/icons/maces/mace_00.png");
+                    temp_int = random(2);
+                    temp_string = "data/textures/UI/icons/maces/mace_";
+                    if (temp_int < 10) temp_string += "0";
+                    temp_string += int_to_string(temp_int) + ".png";
+                    item_pointer->data.image.level_0 = game.texture_manager.add_texture(temp_string);
                 break;
                 case ITEM_SWORD:
                     item_pointer->data.material_type = ITEM_MATERIAL_METAL;
