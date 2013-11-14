7560a82c4b76ff02ca3ac7b24372d7e60a959485
Paul-Wortmann <paul.wortmann@gmail.com>
2013-11-14 23:31:20 +0900

    Hammers to use random texture.

--- source/core/item_manager.cpp
+++ source/core/item_manager.cpp
@@ -3110,7 +3110,11 @@ void  item_manager_class::gen_item_texture(item_type* item_pointer,int item_type
                 break;
                 case ITEM_HAMMER:
                     item_pointer->data.material_type = ITEM_MATERIAL_METAL;
-                    item_pointer->data.image.level_0 = game.texture_manager.add_texture("data/textures/UI/icons/hammers/hammer_01.png");
+                    temp_int = random(2);
+                    temp_string = "data/textures/UI/icons/hammers/hammer_";
+                    if (temp_int < 10) temp_string += "0";
+                    temp_string += int_to_string(temp_int) + ".png";
+                    item_pointer->data.image.level_0 = game.texture_manager.add_texture(temp_string);
                 break;
                 case ITEM_AXE:
                     item_pointer->data.material_type = ITEM_MATERIAL_METAL;
