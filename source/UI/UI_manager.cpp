f1af85c2f8fd61faf40f13d62fc700b958374635
Paul-Wortmann <paul.wortmann@gmail.com>
2013-11-17 23:42:48 +0900

    Spells draw socketed gem on mouse over.

--- source/UI/UI_manager.cpp
+++ source/UI/UI_manager.cpp
@@ -737,6 +737,9 @@ void UI_manager_class::render(void)
                                         int effect_count = 0;
                                         socket_item_pointer = game.item_manager.add_item(item_pointer->data.socket[socket_count].value);
                                         effect_pointer = game.effect_manager.add_effect(socket_item_pointer->data.effect[effect_count].type);
+                                        texture_pointer = socket_item_pointer->data.image.level_0;
+                                        texture_pointer->data.render_positioning = TEXTURE_RENDER_DOWN+TEXTURE_RENDER_LEFT;
+                                        game.texture_manager.draw(texture_pointer,false,texture_temp_x,texture_temp_y-texture_background_padding*4.5,UI_form_pointer->data.element[element_number].position.z,texture_background_padding,texture_background_padding);
                                         if (socket_item_pointer->data.effect[effect_count].value >  0) sign_string = "+";
                                         if (socket_item_pointer->data.effect[effect_count].value == 0) sign_string = " ";
                                         if (socket_item_pointer->data.effect[effect_count].value <  0) sign_string = "-";
