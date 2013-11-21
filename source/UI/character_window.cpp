da399e563701fdf7ac385840f123639cce5d4ff5
Paul-Wortmann <paul.wortmann@gmail.com>
2013-11-21 23:53:36 +0900

    Display player strength in character window.

--- source/UI/character_window.cpp
+++ source/UI/character_window.cpp
@@ -87,7 +87,7 @@ void setup_character_window(int UID)
     UI_form_pointer->data.title_bar.position.y        = UI_form_pointer->data.position.y+(UI_form_pointer->data.size.y/2.0f)-(UI_form_pointer->data.title_bar.size.y/2.0f);
 
     // ---------------------------- Setup window elements ----------------------------------------------------
-    UI_form_pointer->data.number_of_elements = 8;
+    UI_form_pointer->data.number_of_elements = 11;
     UI_form_pointer->data.element = new UI_element_struct[UI_form_pointer->data.number_of_elements];
     for (int element_count = 0; element_count < UI_form_pointer->data.number_of_elements; element_count++)
     {
@@ -202,8 +202,6 @@ void setup_character_window(int UID)
     UI_form_pointer->data.element[element_number].title.text                  = new texture_type;
     game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Name: ",0.8f,255,255,255,255,TEXTURE_RENDER_LEFT);
     UI_form_pointer->data.element[element_number].title.enabled               = true;
-    UI_form_pointer->data.element[element_number].title.size.y                = 28;
-    UI_form_pointer->data.element[element_number].title.size.x                = 0;//((float)UI_form_pointer->data.element[element_number].title.size.y / 10.0f) * (float)UI_form_pointer->data.element[element_number].title.text.length();
     UI_form_pointer->data.element[element_number].title.position.x            = UI_form_pointer->data.position.x - (UI_form_pointer->data.size.x * 0.1f);
     UI_form_pointer->data.element[element_number].title.position.y            = UI_form_pointer->data.position.y + (UI_form_pointer->data.size.y * 0.32f);
     UI_form_pointer->data.element[element_number].active                      = true;
@@ -213,10 +211,6 @@ void setup_character_window(int UID)
     UI_form_pointer->data.element[element_number].tooltip.enabled             = false;
     UI_form_pointer->data.element[element_number].color                       = UI_form_pointer->data.color;
     UI_form_pointer->data.element[element_number].color.normal                = UI_form_pointer->data.element[element_number].color.highlighted;
-    UI_form_pointer->data.element[element_number].size.x                      = 0.16f;
-    UI_form_pointer->data.element[element_number].size.y                      = UI_form_pointer->data.element[element_number].size.x;
-    UI_form_pointer->data.element[element_number].position.x                  = UI_form_pointer->data.position.x+(UI_form_pointer->data.element[element_number].size.x)-(UI_form_pointer->data.size.x/2.0f);
-    UI_form_pointer->data.element[element_number].position.y                  = UI_form_pointer->data.position.y+(UI_form_pointer->data.element[element_number].size.y * 1.44f);
     UI_form_pointer->data.element[element_number].sound                       = UI_form_pointer->data.sound;
     UI_form_pointer->data.element[element_number].sound.on_click.enabled      = false;
     UI_form_pointer->data.element[element_number].sound.on_mouse_over.enabled = false;
@@ -226,10 +220,8 @@ void setup_character_window(int UID)
     UI_form_pointer->data.element[element_number].window_UID                  = UI_form_pointer->data.UID;
     UI_form_pointer->data.element[element_number].element_UID                 = element_number;
     UI_form_pointer->data.element[element_number].title.text                  = new texture_type;
-    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Level: ",0.6f,255,255,255,255,TEXTURE_RENDER_LEFT);
+    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Level: ",0.75f,255,255,255,255,TEXTURE_RENDER_LEFT);
     UI_form_pointer->data.element[element_number].title.enabled               = true;
-    UI_form_pointer->data.element[element_number].title.size.y                = 28;
-    UI_form_pointer->data.element[element_number].title.size.x                = 0;//((float)UI_form_pointer->data.element[element_number].title.size.y / (float)UI_form_pointer->data.element[element_number].title.text.length()) * 4.5f;
     UI_form_pointer->data.element[element_number].title.position.x            = UI_form_pointer->data.position.x - (UI_form_pointer->data.size.x * 0.1f);
     UI_form_pointer->data.element[element_number].title.position.y            = UI_form_pointer->data.position.y + (UI_form_pointer->data.size.y * 0.28f);
     UI_form_pointer->data.element[element_number].active                      = true;
@@ -239,10 +231,6 @@ void setup_character_window(int UID)
     UI_form_pointer->data.element[element_number].tooltip.enabled             = false;
     UI_form_pointer->data.element[element_number].color                       = UI_form_pointer->data.color;
     UI_form_pointer->data.element[element_number].color.normal                = UI_form_pointer->data.element[element_number].color.highlighted;
-    UI_form_pointer->data.element[element_number].size.x                      = 0.16f;
-    UI_form_pointer->data.element[element_number].size.y                      = UI_form_pointer->data.element[element_number].size.x;
-    UI_form_pointer->data.element[element_number].position.x                  = UI_form_pointer->data.position.x+(UI_form_pointer->data.element[element_number].size.x)-(UI_form_pointer->data.size.x/2.0f);
-    UI_form_pointer->data.element[element_number].position.y                  = UI_form_pointer->data.position.y+(UI_form_pointer->data.element[element_number].size.y * 1.44f);
     UI_form_pointer->data.element[element_number].sound                       = UI_form_pointer->data.sound;
     UI_form_pointer->data.element[element_number].sound.on_click.enabled      = false;
     UI_form_pointer->data.element[element_number].sound.on_mouse_over.enabled = false;
@@ -252,10 +240,8 @@ void setup_character_window(int UID)
     UI_form_pointer->data.element[element_number].window_UID                  = UI_form_pointer->data.UID;
     UI_form_pointer->data.element[element_number].element_UID                 = element_number;
     UI_form_pointer->data.element[element_number].title.text                  = new texture_type;
-    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Exp: ",0.6f,255,255,255,255,TEXTURE_RENDER_LEFT);
+    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Exp: ",0.75f,255,255,255,255,TEXTURE_RENDER_LEFT);
     UI_form_pointer->data.element[element_number].title.enabled               = true;
-    UI_form_pointer->data.element[element_number].title.size.y                = 28;
-    UI_form_pointer->data.element[element_number].title.size.x                = 0;//((float)UI_form_pointer->data.element[element_number].title.size.y / (float)UI_form_pointer->data.element[element_number].title.text.length()) * 4.5f;
     UI_form_pointer->data.element[element_number].title.position.x            = UI_form_pointer->data.position.x - (UI_form_pointer->data.size.x * 0.1f);
     UI_form_pointer->data.element[element_number].title.position.y            = UI_form_pointer->data.position.y + (UI_form_pointer->data.size.y * 0.24f);
     UI_form_pointer->data.element[element_number].active                      = true;
@@ -265,10 +251,6 @@ void setup_character_window(int UID)
     UI_form_pointer->data.element[element_number].tooltip.enabled             = false;
     UI_form_pointer->data.element[element_number].color                       = UI_form_pointer->data.color;
     UI_form_pointer->data.element[element_number].color.normal                = UI_form_pointer->data.element[element_number].color.highlighted;
-    UI_form_pointer->data.element[element_number].size.x                      = 0.16f;
-    UI_form_pointer->data.element[element_number].size.y                      = UI_form_pointer->data.element[element_number].size.x;
-    UI_form_pointer->data.element[element_number].position.x                  = UI_form_pointer->data.position.x+(UI_form_pointer->data.element[element_number].size.x)-(UI_form_pointer->data.size.x/2.0f);
-    UI_form_pointer->data.element[element_number].position.y                  = UI_form_pointer->data.position.y+(UI_form_pointer->data.element[element_number].size.y * 1.44f);
     UI_form_pointer->data.element[element_number].sound                       = UI_form_pointer->data.sound;
     UI_form_pointer->data.element[element_number].sound.on_click.enabled      = false;
     UI_form_pointer->data.element[element_number].sound.on_mouse_over.enabled = false;
@@ -278,10 +260,8 @@ void setup_character_window(int UID)
     UI_form_pointer->data.element[element_number].window_UID                  = UI_form_pointer->data.UID;
     UI_form_pointer->data.element[element_number].element_UID                 = element_number;
     UI_form_pointer->data.element[element_number].title.text                  = new texture_type;
-    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Next: ",0.6f,255,255,255,255,TEXTURE_RENDER_LEFT);
+    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Next: ",0.75f,255,255,255,255,TEXTURE_RENDER_LEFT);
     UI_form_pointer->data.element[element_number].title.enabled               = true;
-    UI_form_pointer->data.element[element_number].title.size.y                = 28;
-    UI_form_pointer->data.element[element_number].title.size.x                = 0;//((float)UI_form_pointer->data.element[element_number].title.size.y / (float)UI_form_pointer->data.element[element_number].title.text.length()) * 4.5f;
     UI_form_pointer->data.element[element_number].title.position.x            = UI_form_pointer->data.position.x - (UI_form_pointer->data.size.x * 0.1f);
     UI_form_pointer->data.element[element_number].title.position.y            = UI_form_pointer->data.position.y + (UI_form_pointer->data.size.y * 0.20f);
     UI_form_pointer->data.element[element_number].active                      = true;
@@ -291,10 +271,26 @@ void setup_character_window(int UID)
     UI_form_pointer->data.element[element_number].tooltip.enabled             = false;
     UI_form_pointer->data.element[element_number].color                       = UI_form_pointer->data.color;
     UI_form_pointer->data.element[element_number].color.normal                = UI_form_pointer->data.element[element_number].color.highlighted;
-    UI_form_pointer->data.element[element_number].size.x                      = 0.16f;
-    UI_form_pointer->data.element[element_number].size.y                      = UI_form_pointer->data.element[element_number].size.x;
-    UI_form_pointer->data.element[element_number].position.x                  = UI_form_pointer->data.position.x+(UI_form_pointer->data.element[element_number].size.x)-(UI_form_pointer->data.size.x/2.0f);
-    UI_form_pointer->data.element[element_number].position.y                  = UI_form_pointer->data.position.y+(UI_form_pointer->data.element[element_number].size.y * 1.44f);
+    UI_form_pointer->data.element[element_number].sound                       = UI_form_pointer->data.sound;
+    UI_form_pointer->data.element[element_number].sound.on_click.enabled      = false;
+    UI_form_pointer->data.element[element_number].sound.on_mouse_over.enabled = false;
+    UI_form_pointer->data.element[element_number].mouse_delay.maximum         = 30;
+    UI_form_pointer->data.element[element_number].font                        = UI_form_pointer->data.font;
+    element_number = 6; //--- Player Strength ---
+    UI_form_pointer->data.element[element_number].window_UID                  = UI_form_pointer->data.UID;
+    UI_form_pointer->data.element[element_number].element_UID                 = element_number;
+    UI_form_pointer->data.element[element_number].title.text                  = new texture_type;
+    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Strength: ",0.75f,255,255,255,255,TEXTURE_RENDER_LEFT);
+    UI_form_pointer->data.element[element_number].title.enabled               = true;
+    UI_form_pointer->data.element[element_number].title.position.x            = UI_form_pointer->data.position.x - (UI_form_pointer->data.size.x * 0.38f);
+    UI_form_pointer->data.element[element_number].title.position.y            = UI_form_pointer->data.position.y + (UI_form_pointer->data.size.y * 0.14f);
+    UI_form_pointer->data.element[element_number].active                      = true;
+    UI_form_pointer->data.element[element_number].click_enabled               = false;
+    UI_form_pointer->data.element[element_number].type                        = UI_ELEMENT_TEXTLABEL;
+    UI_form_pointer->data.element[element_number].zoom.enabled                = false;
+    UI_form_pointer->data.element[element_number].tooltip.enabled             = false;
+    UI_form_pointer->data.element[element_number].color                       = UI_form_pointer->data.color;
+    UI_form_pointer->data.element[element_number].color.normal                = UI_form_pointer->data.element[element_number].color.highlighted;
     UI_form_pointer->data.element[element_number].sound                       = UI_form_pointer->data.sound;
     UI_form_pointer->data.element[element_number].sound.on_click.enabled      = false;
     UI_form_pointer->data.element[element_number].sound.on_mouse_over.enabled = false;
@@ -315,6 +311,8 @@ void update_character_window(UI_form_struct *UI_form_pointer)
     game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Exp: " + ullint_to_string(game.player.level.current_experience),0.75f,255,255,255,255,TEXTURE_RENDER_LEFT);
     element_number = 5; //--- Player Experience for next level ---
     game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Next: " + ullint_to_string(game.player.level.experience[game.player.level.current+1]),0.75f,255,255,255,255,TEXTURE_RENDER_LEFT);
+    element_number = 6; //--- Player Strength ---
+    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Strength: " + ullint_to_string(game.player.stats.strength),0.75f,255,255,255,255,TEXTURE_RENDER_LEFT);
 };
 
 void process_character_window(UI_form_struct *UI_form_pointer)
