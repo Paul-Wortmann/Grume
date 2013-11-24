9e49fca28df2fb625b9757f40c9df84bbab273a4
Paul-Wortmann <paul.wortmann@gmail.com>
2013-11-24 12:27:44 +0900

    Display character armor

--- source/UI/character_window.cpp
+++ source/UI/character_window.cpp
@@ -456,6 +456,29 @@ void setup_character_window(int UID)
     UI_form_pointer->data.element[element_number].sound.on_mouse_over.enabled = false;
     UI_form_pointer->data.element[element_number].mouse_delay.maximum         = 30;
     UI_form_pointer->data.element[element_number].font                        = UI_form_pointer->data.font;
+
+    element_number = 15; //--- Player armor ---
+    UI_form_pointer->data.element[element_number].window_UID                  = UI_form_pointer->data.UID;
+    UI_form_pointer->data.element[element_number].element_UID                 = element_number;
+    UI_form_pointer->data.element[element_number].title.text                  = new texture_type;
+    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Armor: " + ullint_to_string(game.player.stats.damage),0.75f,255,255,255,255,TEXTURE_RENDER_LEFT);
+    UI_form_pointer->data.element[element_number].title.enabled               = true;
+    UI_form_pointer->data.element[element_number].title.position.x            = UI_form_pointer->data.position.x + (UI_form_pointer->data.size.x * 0.036f);
+    UI_form_pointer->data.element[element_number].title.position.y            = UI_form_pointer->data.position.y + (UI_form_pointer->data.size.y * 0.14f);
+    UI_form_pointer->data.element[element_number].active                      = true;
+    UI_form_pointer->data.element[element_number].click_enabled               = false;
+    UI_form_pointer->data.element[element_number].type                        = UI_ELEMENT_TEXTLABEL;
+    UI_form_pointer->data.element[element_number].zoom.enabled                = false;
+    UI_form_pointer->data.element[element_number].tooltip.enabled             = false;
+    UI_form_pointer->data.element[element_number].color                       = UI_form_pointer->data.color;
+    UI_form_pointer->data.element[element_number].color.normal                = UI_form_pointer->data.element[element_number].color.highlighted;
+    UI_form_pointer->data.element[element_number].sound                       = UI_form_pointer->data.sound;
+    UI_form_pointer->data.element[element_number].sound.on_click.enabled      = false;
+    UI_form_pointer->data.element[element_number].sound.on_mouse_over.enabled = false;
+    UI_form_pointer->data.element[element_number].mouse_delay.maximum         = 30;
+    UI_form_pointer->data.element[element_number].font                        = UI_form_pointer->data.font;
+
+
 };
 
 void update_character_window(UI_form_struct *UI_form_pointer)
@@ -491,7 +514,16 @@ void update_character_window(UI_form_struct *UI_form_pointer)
     element_number = 14; //--- Player Damage - Lightning ---
     game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Lightning Damage: " + ullint_to_string(game.player.stats.damage_max),0.75f,255,255,127,255,TEXTURE_RENDER_LEFT);
 
+    element_number = 15; //--- Player armor ---
+    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Armor: " + ullint_to_string(game.player.stats.damage),0.75f,255,255,255,255,TEXTURE_RENDER_LEFT);
 
+//    player_class::stats.armor                   = 0;
+/*
+    player_class::stats.resist_physical         = 0;
+    player_class::stats.resist_ice              = 0;
+    player_class::stats.resist_fire             = 0;
+    player_class::stats.resist_lightning        = 0;
+*/
 };
 
 void process_character_window(UI_form_struct *UI_form_pointer)
