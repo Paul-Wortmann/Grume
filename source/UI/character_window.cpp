6a38f70d1fe8e5840e6f70b3e6ab5eaadc7515bf
Paul-Wortmann <paul.wortmann@gmail.com>
2013-11-25 22:36:56 +0900

    Display movement speed stat.

--- source/UI/character_window.cpp
+++ source/UI/character_window.cpp
@@ -859,6 +859,26 @@ void setup_character_window(int UID)
     UI_form_pointer->data.element[element_number].sound.on_mouse_over.enabled = false;
     UI_form_pointer->data.element[element_number].mouse_delay.maximum         = 30;
     UI_form_pointer->data.element[element_number].font                        = UI_form_pointer->data.font;
+    element_number = 35; //--- Player movement speed ---
+    UI_form_pointer->data.element[element_number].window_UID                  = UI_form_pointer->data.UID;
+    UI_form_pointer->data.element[element_number].element_UID                 = element_number;
+    UI_form_pointer->data.element[element_number].title.text                  = new texture_type;
+    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Movement speed: ",0.75f,191,191,191,255,TEXTURE_RENDER_LEFT);
+    UI_form_pointer->data.element[element_number].title.enabled               = true;
+    UI_form_pointer->data.element[element_number].title.position.x            = UI_form_pointer->data.position.x + (UI_form_pointer->data.size.x * 0.036f);
+    UI_form_pointer->data.element[element_number].title.position.y            = UI_form_pointer->data.position.y - (UI_form_pointer->data.size.y * 0.20f);
+    UI_form_pointer->data.element[element_number].active                      = true;
+    UI_form_pointer->data.element[element_number].click_enabled               = false;
+    UI_form_pointer->data.element[element_number].type                        = UI_ELEMENT_TEXTLABEL;
+    UI_form_pointer->data.element[element_number].zoom.enabled                = false;
+    UI_form_pointer->data.element[element_number].tooltip.enabled             = false;
+    UI_form_pointer->data.element[element_number].color                       = UI_form_pointer->data.color;
+    UI_form_pointer->data.element[element_number].color.normal                = UI_form_pointer->data.element[element_number].color.highlighted;
+    UI_form_pointer->data.element[element_number].sound                       = UI_form_pointer->data.sound;
+    UI_form_pointer->data.element[element_number].sound.on_click.enabled      = false;
+    UI_form_pointer->data.element[element_number].sound.on_mouse_over.enabled = false;
+    UI_form_pointer->data.element[element_number].mouse_delay.maximum         = 30;
+    UI_form_pointer->data.element[element_number].font                        = UI_form_pointer->data.font;
 
 
 };
@@ -934,6 +954,8 @@ void update_character_window(UI_form_struct *UI_form_pointer)
     game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Gold find: " + ullint_to_string(game.player.stats.gold_find),0.75f,191,191,191,255,TEXTURE_RENDER_LEFT);
     element_number = 34; //--- Player light radius ---
     game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Light radius: " + ullint_to_string(game.player.stats.light_radius),0.75f,191,191,191,255,TEXTURE_RENDER_LEFT);
+    element_number = 35; //--- Player movement speed ---
+    game.texture_manager.load_string(UI_form_pointer->data.element[element_number].title.text,game.font_manager.root,"Movement: " + ullint_to_string(game.player.stats.movement_speed),0.75f,191,191,191,255,TEXTURE_RENDER_LEFT);
 
 
 /*
