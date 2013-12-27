9792087cde63b0e4eac501edd75472807df486d7
Paul-Wortmann <paul.wortmann@gmail.com>
2013-12-27 20:27:48 +0900

    calc start tile for map gen M1

--- source/core/map_gen_M1.cpp
+++ source/core/map_gen_M1.cpp
@@ -36,7 +36,7 @@ void           map_gen_M1              (fmx_map_type *fmx_map_pointer, int seed)
 
 void           map_gen_maze            (fmx_map_type *fmx_map_pointer, int tile_x, int tile_y)
 {
-
+    int start_tile = (tile_y * fmx_map_pointer->data.map_width) + tile_x;
 };
 
 void           map_gen_M1              (fmx_map_type *fmx_map_pointer)
