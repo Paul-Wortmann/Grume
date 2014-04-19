04925b1f9ff16755115762f0e4d3da46949abbbc
Paul-Wortmann <paul.wortmann@gmail.com>
2014-04-19 16:26:44 +0900

    removed unneeded includes.

--- source/main.cpp
+++ source/main.cpp
@@ -25,10 +25,6 @@
 //#include <physfs.h>
 #include "game/game.hpp"
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_opengl.h>
-#include <SDL2/SDL_ttf.h>
 #include "core/misc.hpp"
 
 extern game_class    game;
