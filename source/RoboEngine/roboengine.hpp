f21a19dd51f09dcc760404e3eb365c6743c5eda0
Paul <paul.wortmann@gmail.com>
2016-09-05 23:13:09 +0900

    Remove redundant includes.

--- source/RoboEngine/roboengine.hpp
+++ source/RoboEngine/roboengine.hpp
@@ -42,10 +42,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
 #include "entity/re_entity.hpp"
 #include "entity/re_entity_physics.hpp"
