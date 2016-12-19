629160b2fea05e9ac18dc9f54e00724d8ef68de9
Paul <paul.wortmann@gmail.com>
2016-12-19 19:24:55 +0900

    Engine defines.

---
+++ source/RoboEngine/core/re_defines.hpp
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) Paul Wortmann, PhysHex Games, www.physhexgames.com
+ * This file is part of "Frost And Flame"
+ *
+ * "Frost And Flame" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 only.
+ *
+ * "Frost And Flame" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Frost And Flame" If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author  Paul Wortmann
+ * @email   physhex@gmail.com
+ * @website www.physhexgames.com
+ * @license GPL V2
+ * @date 2011-11-11
+ */
+
+#ifndef RE_DEFINES_HPP
+#define RE_DEFINES_HPP
+
+#ifndef RE_DEBUG
+#define RE_DEBUG 1
+#endif // RE_DEBUG
+
+#ifndef RE_ENGINE_LOG
+#define RE_ENGINE_LOG "RoboEngine.log"
+#endif // RE_ENGINE_LOG
+
+#endif // RE_DEFINES_HPP
