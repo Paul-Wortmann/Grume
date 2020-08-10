f02e9a65503656912a866869d5a53fa78a354180
Paul-Wortmann <paul.wortmann@gmail.com>
2020-08-10 18:16:45 +0900

    Correct license information.

--- source/version.hpp
+++ source/version.hpp
@@ -1,24 +1,24 @@
 /**
  * Copyright (C) Paul Wortmann, PhysHex Games, www.physhexgames.com
- * This file is part of "libMath"
+ * This file is part of "Frost And Flame"
  *
- * "libMath" is free software: you can redistribute it and/or modify
+ * "Frost And Flame" is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 2 only.
  *
- * "libMath" is distributed in the hope that it will be useful,
+ * "Frost And Flame" is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with "libMath" If not, see <http://www.gnu.org/licenses/>.
+ * along with "Frost And Flame" If not, see <http://www.gnu.org/licenses/>.
  *
  * @author  Paul Wortmann
  * @email   physhex@gmail.com
  * @website www.physhexgames.com
  * @license GPL V2
- * @date 2020-04-23
+ * @date 2011-11-11
  */
 
 #ifndef FNF_VERSION_HPP
