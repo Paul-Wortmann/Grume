3ead70ad7cead0430006d1df50f5649a21757a29
Paul-Wortmann <paul.wortmann@gmail.com>
2021-05-02 18:15:02 +0900

    Update license information.

--- source/player/player_manager.hpp
+++ source/player/player_manager.hpp
@@ -1,5 +1,25 @@
-
-
+/**
+ * Copyright (C) Paul Wortmann, PhysHex Games, www.physhexgames.com
+ * This file is part of "Frost And Flame"
+ *
+ * "Frost And Flame" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 only.
+ *
+ * "Frost And Flame" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Frost And Flame" If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author  Paul Wortmann
+ * @email   physhex@gmail.com
+ * @website www.physhexgames.com
+ * @license GPL V2
+ * @date 2011-11-11
+ */
 
 #ifndef PLAYER_MANAGER_HPP
 #define PLAYER_MANAGER_HPP
