709a4c6a1b0b9ea688096c1091246e1213132bcf
Paul-Wortmann <paul.wortmann@gmail.com>
2022-02-11 21:43:37 +0900

    Code cleanup.

--- source/entity/entity_collision.hpp
+++ source/entity/entity_collision.hpp
@@ -28,8 +28,8 @@
 
 struct sEntityCollision
 {
-    uint32  size = 1;
-    uint32* data = nullptr;
+    std::uint32_t  size = 1;
+    std::uint32_t* data = nullptr;
 };
 
 #endif // ENTITY_COLLISION_HPP
