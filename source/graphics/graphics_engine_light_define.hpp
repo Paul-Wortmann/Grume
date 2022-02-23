f941810e61d79cc80c3e1d3db535cb43f7b90081
Paul-Wortmann <paul.wortmann@gmail.com>
2022-02-23 14:07:40 +0900

    Code cleanup.

--- source/graphics/graphics_engine_light_define.hpp
+++ source/graphics/graphics_engine_light_define.hpp
@@ -40,9 +40,9 @@ struct sGraphicsEnginePointLight
 
     glm::vec3 position     = glm::vec3(0.0f, 0.0f, 0.0f);
 
-    float32   constant     = 0.0f;
-    float32   linear       = 0.0f;
-    float32   quadratic    = 0.0f;
+    float     constant     = 0.0f;
+    float     linear       = 0.0f;
+    float     quadratic    = 0.0f;
 
     glm::vec3 ambient      = glm::vec3(0.0f, 0.0f, 0.0f);
     glm::vec3 diffuse      = glm::vec3(0.0f, 0.0f, 0.0f);
