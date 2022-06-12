bf0dd4e8213d1b64a112ca90b09604b533e87b44
Paul-Wortmann <paul.wortmann@gmail.com>
2022-06-13 08:53:10 +0900

    Add NPC dialog class.

---
+++ source/npc/npc_dialog.cpp
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) Paul Wortmann
+ * This file is part of "Grume"
+ *
+ * "Grume" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 only.
+ *
+ * "Grume" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Grume" If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author  Paul Wortmann
+ * @email   physhex@gmail.com
+ * @website www.physhexgames.com
+ * @license GPL V2
+ * @date 2011-11-11
+ */
+
+#include "npc_dialog.hpp"
+
+void cNPCDialog::initialize(void)
+{
+    
+}
+
+void cNPCDialog::terminate(void)
+{
+    
+}
+
+void cNPCDialog::process(const float &_dt)
+{
+    
+}
+
--- source/npc/npc_dialog.hpp
+++ source/npc/npc_dialog.hpp
@@ -21,46 +21,26 @@
  * @date 2011-11-11
  */
 
-#ifndef NPC_MANAGER_HPP
-#define NPC_MANAGER_HPP
+#ifndef NPC_DIALOG_HPP
+#define NPC_DIALOG_HPP
 
 #include "../core/includes.hpp"
 #include "../core/game_database.hpp"
 #include "../entity/entity_manager.hpp"
-#include "../map/map_define.hpp"
-#include "../map/map_path_a_star.hpp"
-#include "../map/map_line_of_sight.hpp"
 
-#include "npc_dialog.hpp"
-
-class cNPCManager
+class cNPCDialog
 {
     public:
-        void     initialize(cEntityManager* _entityManager);
+        void     initialize(void);
         void     terminate(void);
         void     process(const float &_dt);
-        void     setEntityHead(sEntity* _entity) { m_entityHead = _entity; }
-        void     setEntityPlayer(sEntity* _entity) { m_entityPlayer = _entity; }
-        void     setDatabasePointer(cGameDatabase* _gameDatabase) { m_gameDatabase = _gameDatabase; };
-        void     setMapPointer(sMap* _map) { m_mapPointer = _map; };
 
     protected:
 
     private:
-        // Entity management
-        cEntityManager* m_entityManager = nullptr;
         sEntity*        m_entityHead    = nullptr;
         sEntity*        m_entityTemp    = nullptr;
         sEntity*        m_entityPlayer  = nullptr;
-        
-        cGameDatabase*  m_gameDatabase  = nullptr;
-        sMap*           m_mapPointer    = nullptr;
-        
-        cNPCDialog      m_npcDialog     = {};
-
-        std::uint32_t   m_positionToTile(glm::vec3 _position);
-        glm::vec3       m_tileToPosition(std::uint32_t _tile);
-        void            m_generateWaypoints(sEntity*& _entity);
 };
 
-#endif //NPC_MANAGER_HPP
+#endif //NPC_DIALOG_HPP
